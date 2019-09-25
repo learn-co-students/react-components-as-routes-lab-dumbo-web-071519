3c742e37406bd490eb53c3f232526de3ef8df186
Ashna Shah-Grover <as19@williams.edu>
2019-09-25 10:56:27 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,14 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map(actor=>{
+        return <div>
+          <h2>{actor.name}</h2>
+          Movies:<br/>
+          {actor.movies.map(movie=>{return <li>{movie}</li>})}
+        </div>
+      })}
     </div>
   );
 };
